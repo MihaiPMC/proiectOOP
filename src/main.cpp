0beb862a906c1dc21893bdc4687623387e8a8384
Mihai Patru <mihaipatru05@gmail.com>
2025-04-24 01:24:25 +0300

    Update CMake configuration to support SFML and set C++17 standard

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <array>
+#include <chrono>
+#include <thread>
+
+#include <SFML/Graphics.hpp>
+
+                                    //
+
+
+
+int main() {
+
+    sf::RenderWindow window;
+    ///////////////////////////////////////////////////////////////////////////
+    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
+    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
+    /// This is needed so we do not burn the GPU                            ///
+    window.setVerticalSyncEnabled(true);                                    ///
+    /// window.setFramerateLimit(60);                                       ///
+    ///////////////////////////////////////////////////////////////////////////
+
+    while(window.isOpen()) {
+        bool shouldExit = false;
+        sf::Event e{};
+        while(window.pollEvent(e)) {
+            switch(e.type) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::Resized:
+                std::cout << "New width: " << window.getSize().x << '\n'
+                          << "New height: " << window.getSize().y << '\n';
+                break;
+            case sf::Event::KeyPressed:
+                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
+                if(e.key.code == sf::Keyboard::Escape)
+                    shouldExit = true;
+                break;
+            default:
+                break;
+            }
+        }
+        if(shouldExit) {
+            window.close();
+            break;
+        }
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(300ms);
+
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
